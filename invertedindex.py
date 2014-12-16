a6ab174fefb7dcf021f53df8fe160eaa33acb585
Shashank S. Belvadi <shashanksbelvadi@gmail.com>
2014-12-15 18:43:26 -0800

    Ability to search in the inverted index.

--- invertedindex.py
+++ invertedindex.py
@@ -9,3 +9,11 @@ class InvertedIndex(object):
       InvertedIndex.ind[word] = filepaths
     else:
       InvertedIndex.ind[word].append(filepath)
+
+  @staticmethod
+  def searchword(word):
+    if word not in InvertedIndex.ind:
+      return []
+    else:
+      return InvertedIndex.ind[w]
+      
